b344ae177e209f6ddb252713debcbfe6fa92dc9b
pratikdeore <58384739+pratikdeore@users.noreply.github.com>
2021-01-05 21:58:13 +0530

    updated

---
+++ index.js
@@ -0,0 +1,105 @@
+function changeimage1() {
+    document.getElementById("logo1").src = "imgs/logo1.jpeg";
+}
+function changeimage_1() {
+    document.getElementById("logo1").src = "imgs/logo_1.jpeg";
+}
+
+
+
+function changeimage4() {
+    document.getElementById("logo4").src = "imgs/logo4.jpeg";
+}
+function changeimage_4() {
+    document.getElementById("logo4").src = "imgs/logo_4.png";
+}
+
+
+
+function changeimage2() {
+    document.getElementById("logo2").src = "imgs/logo2.jpeg";
+}
+function changeimage_2() {
+    document.getElementById("logo2").src = "imgs/logo_2.jpeg";
+}
+
+
+
+function changeimage3() {
+    document.getElementById("logo3").src = "imgs/logo3.jpeg";
+}
+function changeimage_3() {
+    document.getElementById("logo3").src = "imgs/logo_3.jpeg";
+}
+
+
+
+function changeimage13() {
+    document.getElementById("logo13").src = "imgs/logo13.jpeg";
+}
+function changeimage_13() {
+    document.getElementById("logo13").src = "imgs/logo_13.jpeg";
+}
+
+
+
+function changeimage6() {
+    document.getElementById("logo6").src = "imgs/logo6.jpeg";
+}
+function changeimage_6() {
+    document.getElementById("logo6").src = "imgs/logo_6.png";
+}
+
+
+
+function changeimage10() {
+    document.getElementById("logo10").src = "imgs/logo10.jpeg";
+}
+function changeimage_10() {
+    document.getElementById("logo10").src = "imgs/logo_10.jpeg";
+}
+
+
+
+function changeimage7() {
+    document.getElementById("logo7").src = "imgs/logo7.jpeg";
+}
+function changeimage_7() {
+    document.getElementById("logo7").src = "imgs/logo_7.jpeg";
+}
+
+
+
+function changeimage5() {
+    document.getElementById("logo5").src = "imgs/logo5.jpeg";
+}
+function changeimage_5() {
+    document.getElementById("logo5").src = "imgs/logo_5.jpg";
+}
+
+
+
+function changeimage8() {
+    document.getElementById("logo8").src = "imgs/logo8.jpeg";
+}
+function changeimage_8() {
+    document.getElementById("logo8").src = "imgs/logo_8.jpeg";
+}
+
+
+
+function changeimage9() {
+    document.getElementById("logo9").src = "imgs/logo9.jpeg";
+}
+function changeimage_9() {
+    document.getElementById("logo9").src = "imgs/logo_9.jpeg";
+}
+
+
+
+function changeimage11() {
+    document.getElementById("logo11").src = "imgs/logo11.jpeg";
+}
+function changeimage_11() {
+    document.getElementById("logo11").src = "imgs/logo_11.jpeg";
+}
